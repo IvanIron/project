d11a67651cc2a1967db8c5f4b308c48cd53d24ee
Ivan <ivan.googllle@gmail.com>
2024-05-30 08:13:13 +0300

    First

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//alert('Hello');
+
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", " ");
+//console.log(answer + 5);
+
+const answer = [];
+
+answer[0] = prompt("What is yor name?", "");
+answer[1] = prompt("What is yor second name?", "");
+answer[2] = prompt("How old are you?", "");
+
+document.write(answer);
