49eebd705ee3a15b2ff6d9147678a3eb591d58ae
Ivan <ivan.googllle@gmail.com>
2024-05-30 08:38:08 +0300

    main

--- js/script.js
+++ js/script.js
@@ -11,8 +11,9 @@
 
 const answer = [];
 
-answer[0] = prompt("What is yor name?", "");
-answer[1] = prompt("What is yor second name?", "");
-answer[2] = prompt("How old are you?", "");
+//answer[0] = prompt("What is yor name?", "");
+//answer[1] = prompt("What is yor second name?", "");
+//answer[2] = prompt("How old are you?", "");
 
-document.write(answer);
+//document.write(answer);
+console.log(typeof(answer));
